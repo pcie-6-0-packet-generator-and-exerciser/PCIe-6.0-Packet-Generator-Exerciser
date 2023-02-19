c649900b3e49bbce93cd6f09f33e3013ee0588b9
Mohanad <mohanadismail789@gmail.com>
2023-02-19 21:41:02 +0200

    PCIE-124 Add initial QueueWrapper implementation

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/queue_wrapper.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/queue_wrapper.cpp
@@ -1,1 +1,40 @@
 #include "queue_wrapper.h"
+
+//constructor
+QueueWrapper::QueueWrapper()
+{
+}
+
+//destructor
+QueueWrapper::~QueueWrapper()
+{
+}
+
+//pushes a TLP to the queue
+void QueueWrapper::push(TLP tlp)
+{
+	std::unique_lock<std::mutex> mlock(mutex);
+	queue.push(tlp);
+	mlock.unlock();
+	condition.notify_one();
+}
+
+//pops a TLP from the queue
+TLP QueueWrapper::pop()
+{
+	std::unique_lock<std::mutex> mlock(mutex);
+	while (queue.empty())
+	{
+		condition.wait(mlock);
+	}
+	auto item = queue.front();
+	queue.pop();
+	return item;
+}
+
+//returns the size of the queue
+int QueueWrapper::size()
+{
+	std::unique_lock<std::mutex> mlock(mutex);
+	return queue.size();
+}
--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/queue_wrapper.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/queue_wrapper.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <queue>
+#include <mutex>
+
+// class QueueWrapper that wraps a queue and implements producer consumer model
+class QueueWrapper
+{
+	//queue
+	std::queue<TLP> queue;
+
+	//mutex
+	std::mutex mutex;
+
+	//condition variable
+	std::condition_variable condition;
+
+	public:
+		//constructor
+		QueueWrapper();
+
+		//destructor
+		~QueueWrapper();
+
+		//pushes a TLP to the queue
+		void push(TLP tlp);
+
+		//pops a TLP from the queue
+		TLP pop();
+
+		//returns the size of the queue
+		int size();
+};
