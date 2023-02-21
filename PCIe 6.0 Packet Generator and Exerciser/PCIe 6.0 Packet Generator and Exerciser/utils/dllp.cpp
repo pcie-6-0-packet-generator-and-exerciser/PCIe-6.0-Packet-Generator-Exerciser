d670d903a17f11a1cc76a9e8e17348b506acc5d6
Nazeeh Maged <nazeehmaged007@gmail.com>
2023-02-21 02:29:55 +0200

    PCIE-79 Fixed Bug in getBitRep

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/dllp.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/dllp.cpp
@@ -19,32 +19,32 @@ std::array<bool, 32> Dllp::getBitRep() const {
     std::array<bool, 32> result;
     int idx = 0;
     std::bitset<12> datafc_bits(DataFC);
-    for (int i = 11; i >= 0; i--) {
+    for (int i = 0; i <12 ; i++) {
         result[idx++] = datafc_bits[i];
     }
     std::bitset<2> datascale_bits(DataScale);
-    for (int i = 1; i >= 0; i--) {
+    for (int i = 0; i < 2 ; i++) {
         result[idx++] = datascale_bits[i];
     }
     std::bitset<8> hdrfc_bits(HdrFC);
-    for (int i = 7; i >= 0; i--) {
+    for (int i = 0; i < 8 ; i++) {
         result[idx++] = hdrfc_bits[i];
     }
     std::bitset<2> hdrscale_bits(HdrScale);
-    for (int i = 1; i >= 0; i--) {
+    for (int i = 0; i < 2 ; i++) {
         result[idx++] = hdrscale_bits[i];
     }
     std::bitset<3> vc_bits(VC);
-    for (int i = 2; i >= 0; i--) {
+    for (int i = 0; i < 3 ; i++) {
         result[idx++] = vc_bits[i];
     }
     result[idx++] = shared;
     std::bitset<2> fctype_bits(static_cast<int>(creditType));
-    for (int i = 1; i >= 0; i--) {
+    for (int i = 0; i < 2 ; i++) {
         result[idx++] = fctype_bits[i];
     }
     std::bitset<2> datatype_bits(static_cast<int>(type));
-    for (int i = 1; i >= 0; i--) {
+    for (int i = 0; i < 2 ; i++) {
         result[idx++] = datatype_bits[i];
     }
     return result;
