82efd645f4d23f71594a5f5cf394287fc766f6cd
Mohanad <mohanadismail789@gmail.com>
2023-02-22 20:47:40 +0200

    PCIE-98 Include dllp.h

--- PCIe 6.0 Packet Generator and Exerciser/Google Test/layers/datalink_unittest.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/Google Test/layers/datalink_unittest.cpp
@@ -1,6 +1,7 @@
 #include "../pch.h"
 #include "../../PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h"
 #include "../../PCIe 6.0 Packet Generator and Exerciser/utils/flit.h"
+#include "../../PCIe 6.0 Packet Generator and Exerciser/utils/dllp.h"
 
 TEST(UpdateCreditLimit, SetPCreditLimit) {
 	const DatalinkLayer datalink;
@@ -10,5 +11,5 @@ TEST(UpdateCreditLimit, SetPCreditLimit) {
 	int NPSharedCreditLimit[2];
 	int CplSharedCreditLimit[2];
 	datalink.updateCreditLimit(new Flit(null, null, null, 
-		new Dllp(1, true, 0, 3, 1, 5, initFC1, P), null, null);
+		new Dllp(1, true, 0, 3, 1, 5, Dllp::DllpType::initFC1, Dllp::CreditType::P), null, null));
 }
