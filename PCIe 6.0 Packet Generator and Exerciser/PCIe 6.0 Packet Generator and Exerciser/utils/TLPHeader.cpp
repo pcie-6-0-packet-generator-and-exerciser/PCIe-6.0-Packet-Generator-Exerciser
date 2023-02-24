acd794b31cff9d4b9ee46f7924aaccdf2fec20ba
Nazeeh Maged <nazeehmaged007@gmail.com>
2023-02-24 05:24:17 +0200

    PCIE-82 Added class implementation

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/TLPHeader.cpp
@@ -0,0 +1,5 @@
+#include "TLPHeader.h"
+
+void TLPHeader::setLength(int newLength) {
+	length = newLength;
+}
---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/TLPHeader.h
@@ -0,0 +1,27 @@
+#pragma once
+//#include "OHC.h";
+//#include "NonHeaderBase.h";
+enum TLPType {
+	MemRead32,
+	MemWrite32,
+	MemRead64,
+	MemWrite64,
+	Cpl,
+	CpID,
+	VendorMsg,
+	ConfigRead0,
+	ConfigWrite0,
+	ConfigRead1,
+	ConfigWrite1
+};
+
+class TLPHeader {
+public:
+	TLPType TLPType_;
+	int TC;
+	int length;
+	std::vector<OHC*> OHCs;
+	NonHeaderBase nonBase;
+
+	void setLength(int newLength);
+};
