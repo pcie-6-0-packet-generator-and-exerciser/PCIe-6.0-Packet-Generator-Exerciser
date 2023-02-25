8ba23b75e6beea4b12ad398b9a0f9cb0176833fc
Mario-Kamel <mario.medhat99@gmail.com>
2023-02-25 19:58:31 +0200

    Add Main Window Skeleton

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/main_window.cpp
@@ -0,0 +1,33 @@
+#include "main_window.h"
+#include "content_widget.h"
+
+#include <QtWidgets/QBoxLayout>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPushButton>
+using namespace Ui;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setMinimumSize(1200, 800);
+    setWindowTitle("PCIe6 Packet Generator and Exerciser");
+    createContentWidget();
+    manageLayout();
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::createToolBar() {
+    toolBar_ = new QToolBar(this);
+
+}
+
+void MainWindow::createContentWidget() {
+    contentWidget_ = new ContentWidget(this);
+}
+
+void MainWindow::manageLayout(){
+    setCentralWidget(contentWidget_);
+}
