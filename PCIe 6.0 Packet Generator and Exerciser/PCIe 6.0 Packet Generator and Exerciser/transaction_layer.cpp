9284fe17b3ea34ec5ee45ec03ccca131845c434b
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-02-25 11:08:56 +0200

    Fix Implementation of gating equation

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/transaction_layer.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/transaction_layer.cpp
@@ -15,16 +15,16 @@ bool TransactionLayer::checkGateEquation(int sharedCreditLimit[2], int shardCred
 	// checking for the Data gate equation
 	int dataSharedCreditLimit = sharedCreditLimit[1];
 	int dataSharedCreditConsumed = shardCreditConsumed[1];
-	int twoPowCreditDataFieldSize = pow(CREDIT_DATA_FIELD_SIZE, 2);
-	
+	int twoPowCreditDataFieldSize = pow(2, CREDIT_DATA_FIELD_SIZE);
+
 	if ((dataSharedCreditLimit - (dataSharedCreditConsumed + tlp.dataConsumption) % twoPowCreditDataFieldSize) % twoPowCreditDataFieldSize > twoPowCreditDataFieldSize / 2)
 		return false;
 
 	// checking for the Header gate equation
 	int headerSharedCreditLimit = sharedCreditLimit[0];
 	int headerSharedCreditConsumed = shardCreditConsumed[0];
-	int twoPowCreditDataFieldSize = pow(CREDIT_HEADER_FIELD_SIZE, 2);
-	
+	int twoPowCreditDataFieldSize = pow(2, CREDIT_HEADER_FIELD_SIZE);
+
 	if ((headerSharedCreditLimit - (headerSharedCreditConsumed + tlp.headerConsumption) % twoPowCreditDataFieldSize) % twoPowCreditDataFieldSize > twoPowCreditDataFieldSize / 2)
 		return false;
 
