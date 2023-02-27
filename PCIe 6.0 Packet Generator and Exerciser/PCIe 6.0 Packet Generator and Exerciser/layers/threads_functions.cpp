b5f58c814852586e6ffe7343f9fd66ddc564ec29
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-02-27 21:51:46 +0200

    Initial Implementation for normalFlowReceiver

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/threads_functions.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/threads_functions.cpp
@@ -5,12 +5,11 @@
  *
  * This function waits on the listenOn queue until a flit is available and sends it to a receiver
  *
- * @param transmitter A reference to the Reciever object
+ * @param receiver A reference to the Reciever object
  * @param globals A reference to the Globals object
  * @param listenOn A QueueWrapper<Sequence> object that holds the flits sent from transmitter
- * @param sendOn A QueueWrapper<Flit> object 
  */
-void ThreadsFunctions::normalFlowReceiver(Receiver& receiver, Globals& globals, QueueWrapper<Flit> listenOn, QueueWrapper<Sequence> sendOn) {
+void ThreadsFunctions::normalFlowReceiver(Receiver& receiver, Globals& globals, QueueWrapper<Flit> listenOn) {
 	// waits on the queue until a flit is available 
 	auto flit = listenOn.pop();
 	
