30ff9d677c249952549659b940c3c40e1ae591ff
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2023-03-01 23:40:10 +0200

    put cards in layout &n scrollable scroll bar goes brrr

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/sequence_browser.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/sequence_browser.cpp
@@ -3,16 +3,17 @@
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QBoxLayout>
 #include <QtWidgets/QScrollBar>
+#include <QtWidgets/QScrollArea>
+
 
 using namespace Ui;
-
 SequenceBrowser::SequenceBrowser(QWidget* parent)
 	: QFrame(parent),
 	sideBar_(nullptr)
 {
 	setMinimumSize(400, 400);
-	createSideBar();
-	createCardsSequence();
+	//createSideBar();
+	//createCardsSequence();
 	manageLayout();
 }
 
@@ -21,9 +22,14 @@ SequenceBrowser::~SequenceBrowser()
 
 }
 void SequenceBrowser::createCardsSequence() {
+	
+
 	for (int i = 0; i < 10; ++i) {
 		cards_.push_back(new TLPCard(this, "Card " + QString::number(i)));
+
+		
 	}
+	
 }
 void SequenceBrowser::createSideBar() 
 {
@@ -32,10 +38,25 @@ void SequenceBrowser::createSideBar()
 
 void SequenceBrowser::manageLayout() 
 {
-	QVBoxLayout* contentLayout = new QVBoxLayout;
+	QHBoxLayout* contentLayout = new QHBoxLayout;
 	contentLayout->setContentsMargins(0, 0, 0, 0);
-	contentLayout->setSpacing(10);
+	contentLayout->setSpacing(0);
+	sideBar_ = new QScrollBar(Qt::Vertical, this);
 	contentLayout->addWidget(sideBar_);
+	//contentLayout->addWidget(new TLPCard(this, "Card "));
+	QVBoxLayout* cardLayout = new QVBoxLayout;
+	cardLayout->setContentsMargins(0, 0, 0, 0);
+	cardLayout->setSpacing(0);
+	//contentLayout->addWidget(textLabel_, Qt::AlignCenter);
+	for (int i = 0; i < 15; ++i) {
+		cardLayout->addWidget(new TLPCard(this, "Card " + QString::number(i)));
 
+	}
+	QScrollArea* scrollArea = new QScrollArea;
+	scrollArea->setBackgroundRole(QPalette::Dark);
+	scrollArea->setWidget(this);
+	scrollArea->setVerticalScrollBar(sideBar_);
+	//setLayout(cardLayout);
+	contentLayout->addLayout(cardLayout);
 	setLayout(contentLayout);
 }
