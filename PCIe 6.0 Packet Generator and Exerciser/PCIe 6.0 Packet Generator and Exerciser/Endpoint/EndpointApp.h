a658c4a16845625e11349a3d5b774c00c94732e5
davidwagih <78867146+david-wagih@users.noreply.github.com>
2023-03-04 21:36:22 +0200

    fix some merge conflicts

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/EndpointApp.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/EndpointApp.h
@@ -5,27 +5,20 @@
 #include "PCIeController.h"
 #include "MemoryMap.h"
 #include "ConfigurationSpace.h"
-#include "MemoryRequest.h"
-#include "ConfigurationRequest.h"
 
 class EndpointApp {
 public:
-    EndpointApp() {};
+    EndpointApp(PCIeController* controller, MemoryMap* memoryMap, ConfigurationSpace* configSpace) :
+        controller_(controller),
+        memoryMap_(memoryMap),
+        configSpace_(configSpace) {}
+
     ~EndpointApp() {};
 
-    void sendPacket(const uint8_t* data, uint32_t length);
-    void receivePacket(uint8_t* data, uint32_t length);
-    uint32_t handleMemoryRead(MemoryRequest* request);
-    uint32_t handleMemoryWrite(MemoryRequest* request);
-    uint32_t handleConfigRead(ConfigurationRequest* request);
-    uint32_t handleConfigWrite(ConfigurationRequest* request);
+    void receiveTlpPacket(TlpPacket packet);
 
 private:
     PCIeController* controller_;
     MemoryMap* memoryMap_;
     ConfigurationSpace* configSpace_;
-    std::vector<uint8_t> bar0Memory_;
-    std::vector<uint8_t> bar1Memory_;
-    std::vector<uint8_t> bar2Memory_;
-    std::vector<uint8_t> bar3Memory_;
 };
