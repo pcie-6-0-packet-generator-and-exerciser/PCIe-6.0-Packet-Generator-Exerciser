e8da95b62c6dd37fd3859e7f8153d2a445d6db8d
Mohanad <mohanadismail789@gmail.com>
2023-03-05 22:37:25 +0200

    PCIE-100 Add initial implementation

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.cpp
@@ -1,8 +1,21 @@
 #include "thread_functions.h"
+#include <thread>
 
 void normalFlowSender(Transmitter& transmitter, QueueWrapper<TLP*>& listenOn) {
 	while (true) {
 		// popAll() waits if the queue is empty
 		transmitter.sendSequence(listenOn.popAll());
 	}
+}
+
+void initilizationSender(Globals globals, QueueWrapper<Sequence> queueListenOn, QueueWrapper<Flit> queueSendOn) {
+	Transmitter transmitter = new Transmitter(globals, NULL, queueSendOn);
+	while (!globals.Fl1) {
+		transmitter.sendInitFC1();
+	}
+	while (!globals.Fl2) {
+		transmitter.sendInitFC2();
+	}
+	std::thread normalFlowSender(globals, queueListenOn, queueSendOn);
+	normalFlowSender.join();
 }
--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.h
@@ -2,7 +2,7 @@
 #include "./globals.h"
 #include "./transmitter.h"
 #include "../utils/queue_wrapper.h"
-
+#include "flit.h"
 
 /** 
  * @brief Listen continuously on a queue until it is not empty, then send the sequence of TLPs on the transmitter.
@@ -10,3 +10,14 @@
  * @param listenOn the queue which is listened on
  */
 void normalFlowSender(Transmitter& transmitter, QueueWrapper<TLP*>& listenOn);
+#include "../utils/queue_wrapper.h"
+#include "globals.h"
+#include "sequence.h"
+
+/**
+ * @brief This function is meant to be used to create a thread for each of the root-complex and endpoint
+ * @param globals Globals object corresponding to either the root-complex or the endpoint
+ * @param queueListenOn The QueueWrapper that the layers will receive TLPs from
+ * @param queueSendOn The QueueWrapper that the layers will send on to layers of the other side
+*/
+void initilizationSender(Globals globals, QueueWrapper<Sequence> queueListenOn, QueueWrapper<Flit> queueSendOn);
