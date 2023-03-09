a5a2da759377d9969b67d5b28d7f51c2e78b13b4
Mohanad <mohanadismail789@gmail.com>
2023-03-09 20:00:37 +0200

    PCIE-98 Add an initial testcase

--- PCIe 6.0 Packet Generator and Exerciser/Google Test/layers/datalink_unittest.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/Google Test/layers/datalink_unittest.cpp
@@ -1,17 +1,28 @@
 #include "../pch.h"
 #include "../../PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h"
-#include "../../PCIe 6.0 Packet Generator and Exerciser/utils/flit.h"
+#include "../../PCIe 6.0 Packet Generator and Exerciser/layers/flit.h"
 #include "../../PCIe 6.0 Packet Generator and Exerciser/utils/dllp.h"
 
 TEST(UpdateCreditLimit, SetPCreditLimit) {
-	const DatalinkLayer datalink;
-	bool Fl1 = false;
-	bool Fl2 = false;
-	int PSharedCreditLimit[2];
-	int NPSharedCreditLimit[2];
-	int CplSharedCreditLimit[2];
-	datalink.updateCreditLimit(new Flit(null, null, null, 
-		new Dllp(1, true, 0, 3, 1, 5, Dllp::DllpType::initFC1, Dllp::CreditType::P), null, null));
+	const DatalinkLayer *datalink;
+	const int PSharedCredit[2] = {8, 5};
+	bool FI1 = false;
+	bool FI2 = false;
+	int PSharedCreditLimit[2] = { -1 };
+	int NPSharedCreditLimit[2] = { -1 };
+	int CplSharedCreditLimit[2] = { -1 };
+	Flit* flit = new Flit();
+	Dllp *dllp = new Dllp(1, 1, PSharedCredit[1], PSharedCredit[0], 0, true, Dllp::DllpType::updateFC, Dllp::CreditType::P);
+	flit->DLLPPayload = dllp->getBitRep();
+	datalink.updateCreditLimit(flit, PSharedCreditLimit, NPSharedCreditLimit, CplSharedCreditLimit, &FI1, &FI2);
+	EXPECT_EQ(PSharedCreditLimit[0], PSharedCredit[0]);
+	EXPECT_EQ(PSharedCreditLimit[1], PSharedCredit[1]);
+	EXPECT_EQ(NPSharedCreditLimit[0], -1);
+	EXPECT_EQ(NPSharedCreditLimit[1], -1);
+	EXPECT_EQ(CplSharedCreditLimit[0], -1);
+	EXPECT_EQ(CplSharedCreditLimit[1], -1);
+	EXPECT_EQ(FI1, false);
+	EXPECT_EQ(FI2, false);
 }
 
 TEST(UpdateCreditLimit, SetNPCreditLimit) {
