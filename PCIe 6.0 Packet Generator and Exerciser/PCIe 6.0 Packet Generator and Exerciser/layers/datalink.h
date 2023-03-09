27c9338710871e83a7cd128abe7473243dac01e4
Mohanad <mohanadismail789@gmail.com>
2023-03-09 19:42:14 +0200

    PCIE-89 Add function docstring

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h
@@ -3,5 +3,15 @@
 #include "../utils/dllp.h"
 
 class DatalinkLayer {
+private:
+	/**
+	 * @brief This function adds a DLLP to a flit
+	 * @param flit The flit that the DLLP is added to
+	 * @param dllpType The enum type of the DLLP
+	 * @param creditType The enum type of the credit to inform the receiver of
+	 * @param shared Whether the credit is shared or dedicated
+	 * @param credit The credit to inform the receiver of
+	 * @return 
+	*/
 	Flit *addDLLP(Flit *flit, Dllp::DllpType dllpType, Dllp::CreditType creditType, bool shared, int credit[]);
 };
