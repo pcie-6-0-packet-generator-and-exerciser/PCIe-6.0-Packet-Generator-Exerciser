8320a8dce12655d13381a86373dd2b67cc102bb5
MohamedEmad <1806046@eng.asu.edu.eg>
2023-03-10 21:31:21 +0200

    Implement the TLP contruction algorithms classes

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/configuration_algorithm_classes/completion_construction_algorithms.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+#include "..\register_class\register.h"
+
+class CompletionConstructionAlgorithms
+{
+    public:
+        boost::dynamic_bitset<> dataToBeReadBits;
+
+        CompletionConstructionAlgorithms();
+
+        virtual TLP constructTLP() = 0;
+};
+
+class CompletionWithData: public CompletionConstructionAlgorithms
+{
+    public:
+        TLP constructTLP() override;
+};
+
+class CompletionWithUR: public CompletionConstructionAlgorithms
+{
+    public:
+        TLP constructTLP() override;
+};
+
+class CompletionWithoutData: public CompletionConstructionAlgorithms
+{
+    public:
+        TLP constructTLP() override;
+};
