b075fe80381af16f261a01f939fc14413a07a24e
MohamedEmad <1806046@eng.asu.edu.eg>
2023-03-10 21:25:07 +0200

    Implement Configuration Space Controller class for Handling Configuration TLPs

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/controller_class/configuration_controller.cpp
@@ -0,0 +1,169 @@
+#include "configuration_controller.h"
+
+/**
+ * @brief Construct a new Configuration Controller and initializing some needed pointers to be used inside it 
+ */
+ConfigurationController::ConfigurationController()
+{
+    configuration = ConfigurationSpace::constructConfigurationSpace();
+    capability =  PCIECapability::constructPCIECapability();
+
+    handler = make_shared<ConfigurationRequestHandler>(configuration, capability);
+    tlpConstructor = make_shared<TLPConstructor>();
+
+    cplD = make_shared<CompletionWithData>();
+    cpl = make_shared<CompletionWithoutData>();
+    cplUR = make_shared<CompletionWithUR>();
+}
+
+/**
+ * @brief Getting the Register number which we need to apply the Configuration request on
+ * 
+ * @param tlp -> The TLP coming to be handled
+ * @return int -> The Register Number to apply the Configuration request on
+ */
+int ConfigurationController::getRegisterNumber(TLP * tlp)
+{
+    /* Here I need a pointer to the ConfigNonHeaderBase class and I should put here instead of (nonBase) */
+    //return tlp->header->nonBase->registerNumber;
+}
+
+/**
+ * @brief Knowing whether the Configuration request carries a valid Register number to be handled or not
+ * 
+ * @param registerNumber -> Taken from the coming TLP
+ * @return int -> 1 for valid Register number, 0 otherwise
+ */
+int ConfigurationController::isValidRegisterNumber(int registerNumber)
+{
+    /* This number will be changed when the PCIE Capability Structure ends */
+    if(registerNumber <= 16)
+        return 1;
+    
+    return 0;
+}
+
+/**
+ * @brief Identifying the Configuration request type
+ * 
+ * @param tlp -> The TLP coming to be handled
+ * @return TLPType -> Configuration Read, or Write
+ */
+TLPType ConfigurationController::getTLPType(TLP * tlp)
+{
+    return tlp->header->TLPtype;
+}
+
+/**
+ * @brief Getting the Data Payload (to be written in a Register) from the coming TLP in case of a Configuration Write request
+ * 
+ * @param tlp -> The TLP coming to be handled
+ * @return unsigned int -> Data inside the TLP
+ */
+unsigned int ConfigurationController::getTLPData(TLP * tlp)
+{
+    boost::dynamic_bitset<> dataPayloadBits;
+
+    unsigned int dataPayloadUint;
+
+    dataPayloadBits = tlp -> dataPayload;
+
+    dataPayloadUint = convertToUnsignedInt(dataPayloadBits);
+
+    return dataPayloadUint;
+}
+
+/**
+ * @brief Converting the coming data inside the TLP from dynamic_bitset to unsigned int
+ * 
+ * @param bits -> Data Payload inside the TLP
+ * @return unsigned int -> the data converted into unsigned int
+ */
+unsigned int ConfigurationController::convertToUnsignedInt(boost::dynamic_bitset<> bits)
+{
+    unsigned int uintValue = static_cast<unsigned int>(bits.to_ulong());
+
+    return uintValue;
+}
+
+/**
+ * @brief Doing the exact opposite of the above
+ * 
+ * @param uintValue 
+ * @return boost::dynamic_bitset<> 
+ */
+boost::dynamic_bitset<> ConfigurationController::convertToBitSet(unsigned int uintValue)
+{
+    boost::dynamic_bitset<> bits(uintValue);
+
+    return bits;
+}
+
+/**
+ * @brief Handling the coming Configuration request through:
+ * 
+ * Getting Register Number -> Checking its validness -> if it's not valid, construct a completion TLP with Unsupported request
+ * if valid, continue to the next steps:
+ * Getting TLP Type (Configuration Read, or Write)
+ * a) Configuration Read:
+ * Get the Data to be read from the given Register number -> convert this unsigned int data to dynamic_bitset -> Determine the TLP construction
+ * algorithm to be made (in this case will be CplD) -> Perform this algorithm and return the TLP result
+ * 
+ * b) Configuration Write:
+ * Get Data to be written to the given register from the given TLP -> Perform the write operation inside the regiser, and if it's not a valid
+ * write operation construct a completion TLP with Unsupported request and return it, otherwise, Determine the TLP construction
+ * algorithm to be made (in this case will be Cpl) -> Perform this algorithm and return the TLP result 
+ * 
+ * @param tlp -> TLP to be handled
+ * @return TLP -> the resultant TLP
+ */
+TLP ConfigurationController::handleConfigurationRequest(TLP * tlp)
+{
+    int Registernumber, validWriteOperation = 0;
+    TLPType configType;
+
+    boost::dynamic_bitset<> dataToBeReadBits;
+    unsigned int dataToBeReadUint; // data which we will read from the Configuration Space / PCIE Capability structure
+    unsigned int dataToBeWrittenUint; // data which we will write to the Configuration Space / PCIE Capability structure
+
+    /* Send pointers to the TLP */
+    Registernumber = getRegisterNumber(tlp);
+
+    if(!isValidRegisterNumber(Registernumber))
+    {
+        tlpConstructor->setAlgorithm(cplUR);
+        return tlpConstructor->performAlgorithm();
+    }
+
+    configType = getTLPType(tlp);
+
+    switch (configType)
+    {
+    case TLPType::ConfigRead0:
+        dataToBeReadUint = handler->handleConfigurationRead(Registernumber);
+
+        dataToBeReadBits = convertToBitSet(dataToBeReadUint);
+
+        tlpConstructor->setData(dataToBeReadBits);
+        tlpConstructor->setAlgorithm(cplD);
+        
+        return tlpConstructor->performAlgorithm();
+
+    case TLPType::ConfigWrite0:
+        dataToBeWrittenUint = getTLPData(tlp);
+        
+        validWriteOperation = handler->handleConfigurationWrite(Registernumber, dataToBeWrittenUint);
+        
+        /* That should return a TLP to be returned */
+        if(validWriteOperation)
+            tlpConstructor->setAlgorithm(cpl);
+            return tlpConstructor->performAlgorithm();
+        
+        tlpConstructor->setAlgorithm(cplUR);
+        return tlpConstructor->performAlgorithm();
+
+    default:
+        tlpConstructor->setAlgorithm(cplUR);
+        return tlpConstructor->performAlgorithm();
+    }
+}
