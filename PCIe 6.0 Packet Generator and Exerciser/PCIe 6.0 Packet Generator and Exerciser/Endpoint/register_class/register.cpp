2730cf82ad0446c9557c31d9b152bf8c9b62f49f
MohamedEmad <1806046@eng.asu.edu.eg>
2023-03-10 21:09:17 +0200

    Implement Register class

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/register_class/register.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/register_class/register.cpp
@@ -1,5 +1,14 @@
 #include "register.h"
 
+/**
+ * @brief A Constructor for initializing all of the Register attributes (which some of them won't be accessible again)
+ * 
+ * @param v -> Register value
+ * @param n -> Register name
+ * @param t -> Register type
+ * @param i -> Register initial value
+ * @param m -> Register mask which is used so that no Read Only or Hardware Initialized bits are corrupted
+ */
 Register::Register(unsigned int v, Register_Name n, Register_Type t, unsigned int i, unsigned int m):value_(v), name_(n), type_(t), initialValue_(i), mask_(m), next_(nullptr){}
 
 void Register:: setRegisterValue(unsigned int v)
@@ -7,17 +16,7 @@ void Register:: setRegisterValue(unsigned int v)
     value_ = v;
 }
 
-void Register::Register::setRegisterName(Register_Name n)
-{
-    name_ = n;
-}
-
-void Register::setRegisterType(Register_Type t)
-{
-    type_ = t;
-}
-
-void Register::setNext(Register* n)
+void Register::setRegisterNext(Register* n)
 {
     next_ = n;
 }
@@ -37,17 +36,22 @@ Register_Type Register::getRegisterType()
     return type_;
 }
 
-Register* Register::getNext()
+Register* Register::getRegisterNext()
 {
     return next_;
 }
 
-unsigned int Register::getInitialValue()
+/**
+ * @brief This function returns the initial value before any made configurations so that we can know the initial status of the register
+ * 
+ * @return unsigned int -> initial value put by the Endpoint Vendor
+ */
+unsigned int Register::getRegisterInitialValue()
 {
     return initialValue_;
 }
 
-unsigned int Register::getMask()
+unsigned int Register::getRegisterMask()
 {
     return mask_;
 }
