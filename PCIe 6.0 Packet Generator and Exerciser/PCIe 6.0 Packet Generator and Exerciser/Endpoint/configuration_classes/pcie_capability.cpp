840dd1072a9a6c3a39dc5094ca1360449b941579
MohamedEmad <1806046@eng.asu.edu.eg>
2023-03-15 17:10:25 +0200

    Implement the Constructor for the PCIE Capability Structure

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/configuration_classes/pcie_capability.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/Endpoint/configuration_classes/pcie_capability.cpp
@@ -2,8 +2,26 @@
 
 PCIECapability * PCIECapability::pcieCapability = nullptr;
 
+/**
+ * @brief PCIE Capabilities Structure Constructor for initializing all of the needed Registers
+ *        with some predefined values, and to know the meaning of these values in details, you can
+ *        have a look on Configuration-Space-Enumerations.pdf
+*/
 PCIECapability::PCIECapability()
 {
+    /* Notice that any Read Only Register will have 0 in its mask (as it won't be written in) */
+
+    pushRegister(1, PCI_Express_Capabilities_ID, READ_ONLY, 1, 1, 0); // Stores the Capabilities ID assigned by the PCI-SIG (it will be 0x1 as this is the default value till it's given to me)
+
+    pushRegister(128, Next_Cap_Pointer, READ_ONLY, 1, 128, 0); // 128 is its default value as well
+
+    pushRegister(3, PCI_Express_Capabilities_Register, READ_ONLY, 2, 3, 0);
+
+    pushRegister(0, Device_Capabilities, READ_ONLY, 4, 0, 0);
+
+    pushRegister(1, Device_Control, READ_WRITE, 2, 1, 0x11110600);
+
+    pushRegister(0, Device_Status, READ_WRITE, 2, 0, 0x11111110);
 }
 
 PCIECapability::~PCIECapability()
