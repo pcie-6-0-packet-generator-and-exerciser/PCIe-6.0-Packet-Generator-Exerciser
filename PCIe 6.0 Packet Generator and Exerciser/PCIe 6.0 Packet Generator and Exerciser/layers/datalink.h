e7078fae35d6f27a0c470d2eb458de99f1b1b6b7
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-04-07 22:46:21 +0200

    PCIE-91 Implement addCRC

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/datalink.h
@@ -17,7 +17,12 @@ private:
 	 * @return The flit after adding the DLLP
 	*/
 	Flit* addDLLP(Flit* flit, Dllp::DllpType dllpType, Dllp::CreditType creditType, bool shared, int credit[]);
-
+	/**
+	 * @brief Just adds the CRC value to the flit
+	 * @param flit a pointer to the flit to add the CRC to
+	 * @return pointer to a flit with the CRC added
+	*/
+	Flit* addCRC(Flit* flit);
 public:
 	void updateCreditLimit(Flit flit, int P_SHARED_CREDIT_LIMIT[], int NP_SHARED_CREDIT_LIMIT[], int CPL_SHARED_CREDIT_LIMIT[], int P_DEDICATED_CREDIT_LIMIT[], int NP_DEDICATED_CREDIT_LIMIT[], int CPL_DEDICATED_CREDIT_LIMIT[], bool& FI1, bool& FI2);
 	
