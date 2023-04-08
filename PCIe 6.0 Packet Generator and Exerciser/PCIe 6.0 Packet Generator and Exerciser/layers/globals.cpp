b0fd30bdb875276ddb37a415b84a6cb6ef1e4b1a
Mohanad <mohanadismail789@gmail.com>
2023-04-08 20:53:34 +0200

    Add dedicated credit to globals

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/globals.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/globals.cpp
@@ -1,10 +1,13 @@
 #include "globals.h"
 
-Globals::Globals(int pCredit[2], int npCredit[2], int cplCredit[2]) {
+Globals::Globals(int pSharedCredit[2], int npSharedCredit[2], int cplSharedCredit[2], int pDedicatedCredit[2], int npDedicatedCredit[2], int cplDedicatedCredit[2]) {
     // Set P_CREDIT, NP_CREDIT, and CPL_CREDIT attributes
     for (int i = 0; i < 2; i++) {
-        P_CREDIT[i] = pCredit[i];
-        NP_CREDIT[i] = npCredit[i];
-        CPL_CREDIT[i] = cplCredit[i];
+        P_SHARED_CREDIT[i] = pSharedCredit[i];
+        NP_SHARED_CREDIT[i] = npSharedCredit[i];
+        CPL_SHARED_CREDIT[i] = cplSharedCredit[i];
+        P_DEDICATED_CREDIT[i] = pDedicatedCredit[i];
+        NP_DEDICATED_CREDIT[i] = npDedicatedCredit[i];
+        CPL_DEDICATED_CREDIT[i] = cplDedicatedCredit[i];
     }
 }
