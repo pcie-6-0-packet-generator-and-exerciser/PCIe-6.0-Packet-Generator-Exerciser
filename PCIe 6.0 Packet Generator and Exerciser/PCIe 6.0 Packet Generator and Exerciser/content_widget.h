229af66273af5e60cdc9ed147a2ca5e1ae4ba6d3
Mario-Kamel <mario.medhat99@gmail.com>
2023-04-23 20:22:35 +0200

    Add Queues

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/content_widget.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/content_widget.h
@@ -1,28 +1,41 @@
 #pragma once
 
 #include <QtWidgets/QFrame>
-
+#include "utils/queue_wrapper.h"
+#include "utils/queue_wrapper.cpp"
+class TLP;
 class QPushButton;
 
 namespace Ui
 {
+    class SequenceBrowser;
+
     class ContentWidget: public QFrame
     {
         Q_OBJECT
 
     public:
-        explicit ContentWidget(QWidget* parent = nullptr);
+        explicit ContentWidget(QWidget* parent, 
+            QueueWrapper<TLP*>* rootComplexToLayers,
+            QueueWrapper<TLP*>* layersToRootComplex);
         ~ContentWidget();
     private:
         void createHeader();
         void createBody();
         void createFooter();
+        void createSequenceExplorerTab();
+        void createResultExplorerTab();
         void createSubmitButton();
         void manageLayout();
+    public Q_SLOTS:
+        void onSubmitButtonClick();
     private:
         QFrame* header_;
         QFrame* body_;
         QFrame* footer_;
+        SequenceBrowser* sequenceBrowser_;
         QPushButton* submitButton_;
+        QPushButton* sequenceExplorerTab_;
+        QPushButton* resultExplorerTab_;
     };
 }
