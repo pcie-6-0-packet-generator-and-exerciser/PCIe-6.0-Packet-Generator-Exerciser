c3fac9c648fef48f3abd658cb0c06b00100a6337
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-04-24 22:12:05 +0200

    PCIE-109 Implement sendInitFC1

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/transmitter.cpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "../layers/layers_wrapper.h"
+#include "./globals.h"
+#include "../utils/queue_wrapper.h"
+#include "../utils/dllp.h"
+#include "../utils/tlp.h"
+#include "../layers/flit.h"
+
+/**
+ * @brief Represents a transmitter that sends packets over a network.
+ *
+ * This class encapsulates the functionality required to transmit packets over
+ * a network using a credit-based flow control mechanism. It maintains a set of
+ * global parameters that govern the behavior of the transmitter, as well as a
+ * set of wrapper classes that provide access to the various layers of the
+ * network protocol.
+ */
+class Transmitter {
+public:
+	Globals globals;
+	LayersWrapper* layers;
+	QueueWrapper<Flit*>* listenOn;
+	QueueWrapper<Flit*>* sendOn;
+
+	void sendInitFC1(void);
+	void sendInitFC2(void);
+	void sendSequence(vector<TLP>* sequence);
+};
