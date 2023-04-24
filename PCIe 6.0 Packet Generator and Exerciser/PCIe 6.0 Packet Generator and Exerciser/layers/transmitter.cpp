4d5c2f55c292019e79de3172c1878aeafbb4f907
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-04-24 22:14:20 +0200

    PCIE-109 Fix file names

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/transmitter.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/transmitter.cpp
@@ -1,28 +1,9 @@
-#pragma once
-#include "../layers/layers_wrapper.h"
-#include "./globals.h"
-#include "../utils/queue_wrapper.h"
-#include "../utils/dllp.h"
-#include "../utils/tlp.h"
-#include "../layers/flit.h"
-
+#include "transmitter.h"
 /**
- * @brief Represents a transmitter that sends packets over a network.
- *
- * This class encapsulates the functionality required to transmit packets over
- * a network using a credit-based flow control mechanism. It maintains a set of
- * global parameters that govern the behavior of the transmitter, as well as a
- * set of wrapper classes that provide access to the various layers of the
- * network protocol.
- */
-class Transmitter {
-public:
-	Globals globals;
-	LayersWrapper* layers;
-	QueueWrapper<Flit*>* listenOn;
-	QueueWrapper<Flit*>* sendOn;
-
-	void sendInitFC1(void);
-	void sendInitFC2(void);
-	void sendSequence(vector<TLP>* sequence);
-};
+ * @brief Sends an InitFC1 message to the receiver.
+ * @param void
+ * @return void
+*/
+void Transmitter::sendInitFC1(void) {
+	this->layers->sendNOPFlit(this->globals, Dllp::DllpType::initFC1, this->sendOn);
+}
