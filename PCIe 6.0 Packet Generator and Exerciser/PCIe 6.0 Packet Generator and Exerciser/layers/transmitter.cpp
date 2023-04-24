34b366b6314f0f7c9a06ed3be35a3e2aa4d58bc6
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-04-24 22:18:58 +0200

    PCIE-109 add the segnature for sendSequence

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/transmitter.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/transmitter.cpp
@@ -8,3 +8,6 @@ void Transmitter::sendInitFC2(void) {
 	this->layers->sendNOPFlit(this->globals, Dllp::DllpType::initFC2, this->sendOn);
 }
 
+void Transmitter::sendSequence(vector<TLP>* sequence) {
+	
+}
