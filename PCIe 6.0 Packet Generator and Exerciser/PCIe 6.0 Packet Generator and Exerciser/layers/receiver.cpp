35a8d39b1f68cd47ca8d4eeb793e622f3e4f55a9
Nazeeh Maged <nazeehmaged007@gmail.com>
2023-05-04 04:50:59 +0300

    PCIE-119 implement receiver class

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/receiver.cpp
@@ -0,0 +1,19 @@
+#include "receiver.h"
+
+/*Receiver::Receiver(Globals global, LayersWrapper layer, QueueWrapper<Flit> sendOnQueue) {
+	this->globals = global;
+	this->layers = layer;
+	this->sendOn = sendOnQueue;
+}*/
+
+/*
+//nopflit
+void Receiver::receiveInit(Flit flit) {
+	this->layers.receiveNOPFlit(flit, this->globals.P_SHARED_CREDIT_LIMIT, this->globals.NP_SHARED_CREDIT_LIMIT, this->globals.CPL_SHARED_CREDIT_LIMIT, this->globals.P_DEDICATED_CREDIT, this->globals.NP_DEDICATED_CREDIT, this->globals.CPL_DEDICATED_CREDIT, this->globals.Fl1, this->globals.Fl2);
+}
+
+//payloadflit
+void Receiver::receiveFlit(Flit flit) {
+	this->layers.receivePayloadFlit(flit, this->sendOn, this->globals.P_CREDIT, this->globals.NP_CREDIT, this->globals.CPL_CREDIT);
+}
+*/
