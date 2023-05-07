74b692241a1976537af52882af2cf8212564b644
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-05-07 15:06:05 +0300

    PCIE-105 Remove the if statement that access the queue size

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.cpp
@@ -1,10 +1,8 @@
 #include "thread_functions.h"
 
-void noramlFlowSender(Transmitter& transmitter, QueueWrapper<TLP*>& listenOn) {
+void normalFlowSender(Transmitter& transmitter, QueueWrapper<TLP*>& listenOn) {
 	while (true) {
-		// Wait until the queue is not empty
-		if (!listenOn.size()) {
-			transmitter.sendSequence(listenOn.popAll());
-		}
+		// popAll() waits if the queue is empty
+		transmitter.sendSequence(listenOn.popAll());
 	}
 }
