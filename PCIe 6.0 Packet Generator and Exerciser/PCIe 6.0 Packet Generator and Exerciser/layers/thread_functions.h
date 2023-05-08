9b231ad024c8f703e5dc2852d3c090f6d574f35b
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-05-08 11:46:08 +0300

    PCIE-108 Implement normalFlowSender

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.h
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/layers/thread_functions.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "./globals.h"
 #include "./transmitter.h"
+#include "./receiver.h"
 #include "../utils/queue_wrapper.h"
-
+#include "../utils/tlp.h"
+#include "./flit.h"
 
 /** 
  * @brief Listen continuously on a queue until it is not empty, then send the sequence of TLPs on the transmitter.
  * @param transmitter takes the sequence of TLPs and sends them over the network
- * @param listenOn the queue which is listened on
+ * @param listenOn the queue which is listened on for TLPs
  */
 void normalFlowSender(Transmitter& transmitter, QueueWrapper<TLP*>& listenOn);
+
+/**
+ * @brief Listen continuously on a queue until it is not empty, then send the Flit recieved to the transmitter.
+ * @param transmitter takes the Flit and extracts the TLPs from it
+ * @param listenOn the queue which is listened on for Flits
+ */
+void normalFlowReceiver(Receiver& receiver, QueueWrapper<Flit*>& listenOn);
