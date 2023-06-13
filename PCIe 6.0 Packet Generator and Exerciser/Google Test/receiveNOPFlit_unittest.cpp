25d74cea6451960811c89a83890f182fc3d6985a
Nazeeh Maged <nazeehmaged007@gmail.com>
2023-06-13 19:22:02 +0300

    PCIE-118 dummy testcase for receiveNOPFlit

---
+++ PCIe 6.0 Packet Generator and Exerciser/Google Test/receiveNOPFlit_unittest.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "../PCIe 6.0 Packet Generator and Exerciser/layers/layers_wrapper.h"
+#include "../PCIe 6.0 Packet Generator and Exerciser/layers/globals.h"
+
+TEST(receiveNOPFlitsuite, testname) {
+
+
+
+}
