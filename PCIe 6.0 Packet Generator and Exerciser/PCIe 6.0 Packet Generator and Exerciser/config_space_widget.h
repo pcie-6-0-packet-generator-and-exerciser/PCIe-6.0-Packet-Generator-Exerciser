98717c89d589651322811b582edc772f28a162d0
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2023-06-15 02:10:58 +0300

    intial config space headers view

---
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/config_space_widget.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <QtWidgets/QFrame>
+namespace Ui {
+	class ConfigSpaceWidget : public QFrame
+	{
+		Q_OBJECT
+		public:
+			explicit ConfigSpaceWidget(QWidget* parent = nullptr);
+			~ConfigSpaceWidget();
+		
+
+	};
+}
--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/custom_line_edit.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/custom_line_edit.cpp
@@ -11,6 +11,7 @@ using namespace Ui;
  {
 	label = new QLabel(labelText);
     lineEdit = new QLineEdit(content);
+    lineEdit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     lineEdit->setEnabled(!readOnly);
     lineEdit ->setReadOnly(readOnly);
     layout = new QVBoxLayout;
@@ -18,6 +19,8 @@ using namespace Ui;
     layout->addWidget(lineEdit);
     setLayout(layout);
     setFixedSize(width, height);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    //setStyleSheet("QFrame { background-color: rgb(255, 255, 255); border: 1px solid rgb(0, 0, 0); }");
 	
 }
  
