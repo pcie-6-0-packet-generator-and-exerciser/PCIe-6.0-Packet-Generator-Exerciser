8f16552df6f9580809bc9164ea2e545446198263
Mohanad Ismail <mohanadismail789@gmail.com>
2023-06-15 21:50:09 +0300

    PCIE-216 Remove repeated function

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/tlp.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/utils/tlp.cpp
@@ -1,45 +1,6 @@
 #include "tlp.h"
 #include "bitset_utils.h"
-
-int getHeaderLengthByType(TLPType type) {
-	switch (type) {
-	case TLPType::MemRead32:
-		return 12;
-		break;
-	case TLPType::MemWrite32:
-		return 12;
-		break;
-	case TLPType::MemRead64:
-		return 16;
-		break;
-	case TLPType::MemWrite64:
-		return 16;
-		break;
-	case TLPType::Cpl:
-		return 12;
-		break;
-	case TLPType::CplD:
-		return 16;
-		break;
-	case TLPType::VendorMsg:
-		return 12;
-		break;
-	case TLPType::ConfigRead0:
-		return 12;
-		break;
-	case TLPType::ConfigWrite0:
-		return 12;
-		break;
-	case TLPType::ConfigRead1:
-		return 16;
-		break;
-	case TLPType::ConfigWrite1:
-		return 16;
-		break;
-	default:
-		break;
-	}
-}
+#include "../layers/layers_wrapper.cpp"
 
 int TLP::getTotalLength() {
 	int OHCLength = header->OHCVector.size() * 4;
