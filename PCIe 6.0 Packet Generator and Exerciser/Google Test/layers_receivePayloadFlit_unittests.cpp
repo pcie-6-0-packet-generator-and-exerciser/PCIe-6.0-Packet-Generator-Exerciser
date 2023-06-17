3edac6a07af817c16471d9d038e19bcc95d1399a
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-06-17 15:37:57 +0300

    PCIE-123 SingleTlpWithDataPayload

--- PCIe 6.0 Packet Generator and Exerciser/Google Test/layers_receivePayloadFlit_unittests.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/Google Test/layers_receivePayloadFlit_unittests.cpp
@@ -13,8 +13,8 @@ int cplDedicatedCredit[] = { 10, 10 }
 
 #define DEFAULT_CREDIT_VALUES pSharedCredit, npSharedCredit, cplSharedCredit, pDedicatedCredit, npDedicatedCredit, cplDedicatedCredit
 
-#define DEFAULT_MEM_READ32_TLP_PARAMS 0, 0, 0, 0, 0
-#define DEFAULT_MEM_WRITE32_TLP_PARAMS 0, boost::dynamic_bitset<>(), 0, 0, 0, 0, 0
+#define DEFAULT_MEM_READ32_TLP_PARAMS 0,0, 0, 0, 0, 0
+#define DEFAULT_MEM_WRITE32_TLP_PARAMS 1, dynamicBitsetPayload, 0, 0, 0, 0, 0
 
 // Test Suite: ReceivePayloadFlitTestSuite
 // Description: This test suite contains test cases for the receivePayloadFlit() function.
@@ -50,7 +50,7 @@ TEST(ReceivePayloadFlitTestSuite, EmptyTLPPayload) {
 	EXPECT_EQ(globals.CPL_SHARED_CREDIT[1], cplSharedCredit[1]);
 }
 
-TEST(ReceivePayloadFlitTestSuite, SingleFlitTLPPayload) {
+TEST(ReceivePayloadFlitTestSuite, SingleTLPWithoutDataPayload) {
 	// Set up default credit parameters
 	DEFAULT_CREDIT_PARAMS;
 	Globals globals(DEFAULT_CREDIT_VALUES);
@@ -83,7 +83,7 @@ TEST(ReceivePayloadFlitTestSuite, SingleFlitTLPPayload) {
 	EXPECT_EQ(receivedTlp->header->nonBase->getTag(), 0);
 	EXPECT_EQ(dynamic_cast<AddressRouting32Bit*>(receivedTlp->header->nonBase)->address, 0);
 	EXPECT_EQ(receivedTlp->header->OHCVector.size(), 1);
-	
+
 	auto ohca1 = dynamic_cast<OHCA1*>(receivedTlp->header->OHCVector[0]);
 	EXPECT_EQ(ohca1->firstDWBE.to_ulong(), 0);
 	EXPECT_EQ(ohca1->lastDWBE.to_ulong(), 0);
@@ -91,4 +91,49 @@ TEST(ReceivePayloadFlitTestSuite, SingleFlitTLPPayload) {
 	EXPECT_EQ(receivedTlp->creditConsumedType, Dllp::CreditType::NP);
 	EXPECT_EQ(receivedTlp->headerConsumption, 1);
 	EXPECT_EQ(receivedTlp->dataConsumption, 0);
+}
+
+TEST(ReceivePayloadFlitTestSuite, SingleTlpWithDataPayload) {
+	DEFAULT_CREDIT_PARAMS;
+	Globals globals(DEFAULT_CREDIT_VALUES);
+
+	// Set up parameters for sendPayloadFlit
+	QueueWrapper<Flit*> sendOnQueueFlits;
+	std::queue<TLP*> tlps;
+
+	boost::dynamic_bitset<> dynamicBitsetPayload = boost::dynamic_bitset<>(32, 0xffffffff);
+	
+	TLP* tlp = TLP::createMemWrite32Tlp(DEFAULT_MEM_WRITE32_TLP_PARAMS);
+	tlps.push(tlp);
+
+	layersWrapper.sendPayloadFlit(globals, tlps, sendOnQueueFlits);
+
+	// Set up parameters for receivePayloadFlit
+	QueueWrapper<TLP*> sendOnQueueTlps;
+	//std::queue<Flit*> flits;
+
+	auto flits = sendOnQueueFlits.popAll();
+	//flits.push(flit);
+
+	layersWrapper.receivePayloadFlit(globals, flits, sendOnQueueTlps);
+
+	// Check that the TLP was sent
+	EXPECT_EQ(sendOnQueueTlps.size(), 1);
+
+	TLP* receivedTlp = sendOnQueueTlps.pop();
+
+	EXPECT_EQ(receivedTlp->header->TLPtype, TLPType::MemWrite32);
+	EXPECT_EQ(receivedTlp->header->lengthInDoubleWord, 0);
+	EXPECT_EQ(receivedTlp->header->nonBase->requestID, 0);
+	EXPECT_EQ(receivedTlp->header->nonBase->getTag(), 0);
+	EXPECT_EQ(dynamic_cast<AddressRouting32Bit*>(receivedTlp->header->nonBase)->address, 0);
+	EXPECT_EQ(receivedTlp->header->OHCVector.size(), 1);
+
+	auto ohca1 = dynamic_cast<OHCA1*>(receivedTlp->header->OHCVector[0]);
+	EXPECT_EQ(ohca1->firstDWBE.to_ulong(), 0);
+	EXPECT_EQ(ohca1->lastDWBE.to_ulong(), 0);
+
+	EXPECT_EQ(receivedTlp->creditConsumedType, Dllp::CreditType::P);
+	EXPECT_EQ(receivedTlp->headerConsumption, 1);
+	EXPECT_EQ(receivedTlp->dataConsumption, ceil(tlp->header->lengthInDoubleWord / FC_UNIT_SIZE));
 }
