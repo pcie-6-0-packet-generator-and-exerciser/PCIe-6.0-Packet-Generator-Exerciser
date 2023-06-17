5f73a4f91cb6a8e94b0c19815fb5c9042bf1d04d
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2023-06-17 14:58:35 +0300

    registers order fixed in config space widget

--- PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/config_space_widget.cpp
+++ PCIe 6.0 Packet Generator and Exerciser/PCIe 6.0 Packet Generator and Exerciser/config_space_widget.cpp
@@ -19,33 +19,37 @@ ConfigSpaceWidget::ConfigSpaceWidget(Type1Config* type1space , QWidget* parent)
 	string currentReg;
 	int regNum = 0;
 	layout->addWidget(new QLabel("000h"), 0, 0,1,1,Qt::AlignRight);
-	boost::to_string(type1space->readType1Reg(regNum++),currentReg);
-	
-	layout->addWidget(new CustomLineEdit("Device ID", 2*width, height, QString::fromStdString(currentReg), this), 0, 1, 1, 2);
+
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Vendor ID", 2*width, height, QString::fromStdString(currentReg), this),0,3,1,2);
+	layout->addWidget(new CustomLineEdit("Vendor ID", 2 * width, height, QString::fromStdString(currentReg), this), 0, 3, 1, 2);
+	boost::to_string(type1space->readType1Reg(regNum++),currentReg);
+	layout->addWidget(new CustomLineEdit("Device ID", 2*width, height, QString::fromStdString(currentReg), this), 0, 1, 1, 2);
+	
 
 	layout->addWidget(new QLabel("004h"), 1, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Status", 2*width, height, QString::fromStdString(currentReg), this), 1, 1, 1, 2);
+	layout->addWidget(new CustomLineEdit("Command", 2 * width, height, QString::fromStdString(currentReg), this), 1, 3, 1, 2);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Command", 2*width, height, QString::fromStdString(currentReg), this), 1, 3, 1, 2);
+	layout->addWidget(new CustomLineEdit("Status", 2*width, height, QString::fromStdString(currentReg), this), 1, 1, 1, 2);
+	
 
 	layout->addWidget(new QLabel("008h"), 2, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Class Code", 3*width, height, QString::fromStdString(currentReg), this), 2, 1, 1, 3);
-	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("Revision ID", width, height, QString::fromStdString(currentReg), this), 2, 4, 1, 1);
+	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
+	layout->addWidget(new CustomLineEdit("Class Code", 3*width, height, QString::fromStdString(currentReg), this), 2, 1, 1, 3);
+	
 
 	layout->addWidget(new QLabel("00Ch"), 3, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("BIST", width, height, QString::fromStdString(currentReg), this), 3, 1, 1, 1);
-	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Header Type", width, height, QString::fromStdString(currentReg), this), 3, 2, 1, 1);
+	layout->addWidget(new CustomLineEdit("Cache Line Size", width, height, QString::fromStdString(currentReg), this), 3, 4, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("Primary Latency Timer", width, height, QString::fromStdString(currentReg), this), 3, 3, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Cache Line Size", width, height, QString::fromStdString(currentReg), this), 3, 4, 1, 1);
+	layout->addWidget(new CustomLineEdit("Header Type", width, height, QString::fromStdString(currentReg), this), 3, 2, 1, 1);
+	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
+	layout->addWidget(new CustomLineEdit("BIST", width, height, QString::fromStdString(currentReg), this), 3, 1, 1, 1);
+	
 
 	layout->addWidget(new QLabel("010h"), 4, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
@@ -57,33 +61,36 @@ ConfigSpaceWidget::ConfigSpaceWidget(Type1Config* type1space , QWidget* parent)
 
 	layout->addWidget(new QLabel("018h"), 6, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Secondary Latency Timer", width, height, QString::fromStdString(currentReg), this), 6, 1, 1, 1);
-	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Subordinate Bus Number", width, height, QString::fromStdString(currentReg), this), 6, 2, 1, 1);
+	layout->addWidget(new CustomLineEdit("Primary Bus Number", width, height, QString::fromStdString(currentReg), this), 6, 4, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("Secondary Bus Number", width, height, QString::fromStdString(currentReg), this), 6, 3, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Primary Bus Number", width, height, QString::fromStdString(currentReg), this), 6, 4, 1, 1);
+	layout->addWidget(new CustomLineEdit("Subordinate Bus Number", width, height, QString::fromStdString(currentReg), this), 6, 2, 1, 1);
+	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
+	layout->addWidget(new CustomLineEdit("Secondary Latency Timer", width, height, QString::fromStdString(currentReg), this), 6, 1, 1, 1);
+	
 
 	layout->addWidget(new QLabel("01Ch"), 7, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Secondary Status", 2*width, height, QString::fromStdString(currentReg), this), 7, 1, 1, 2);
+	layout->addWidget(new CustomLineEdit("IO Base", width, height, QString::fromStdString(currentReg), this), 7, 4, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("IO Limit", width, height, QString::fromStdString(currentReg), this), 7, 3, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("IO Base", width, height, QString::fromStdString(currentReg), this), 7, 4, 1, 1);
+	layout->addWidget(new CustomLineEdit("Secondary Status", 2*width, height, QString::fromStdString(currentReg), this), 7, 1, 1, 2);
 
 	layout->addWidget(new QLabel("020h"), 8, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Memory Limit", 2*width, height, QString::fromStdString(currentReg), this), 8, 1, 1, 2);
+	layout->addWidget(new CustomLineEdit("Memory Base", 2 * width, height, QString::fromStdString(currentReg), this), 8, 3, 1, 2);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Memory Base", 2*width, height, QString::fromStdString(currentReg), this), 8, 3, 1, 2);
+	layout->addWidget(new CustomLineEdit("Memory Limit", 2*width, height, QString::fromStdString(currentReg), this), 8, 1, 1, 2);
+	
 
 	layout->addWidget(new QLabel("024h"), 9, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Prefetchable Memory Limit", 2*width, height, QString::fromStdString(currentReg), this), 9, 1, 1, 2);
+	layout->addWidget(new CustomLineEdit("Prefetchable Memory Base", 2 * width, height, QString::fromStdString(currentReg), this), 9, 3, 1, 2);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Prefetchable Memory Base", 2*width, height, QString::fromStdString(currentReg), this), 9, 3, 1, 2);
+	layout->addWidget(new CustomLineEdit("Prefetchable Memory Limit", 2*width, height, QString::fromStdString(currentReg), this), 9, 1, 1, 2);
+	
 
 	layout->addWidget(new QLabel("028h"), 10, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
@@ -95,15 +102,16 @@ ConfigSpaceWidget::ConfigSpaceWidget(Type1Config* type1space , QWidget* parent)
 
 	layout->addWidget(new QLabel("030h"), 12, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("IO Limit Upper 16 Bits", width, height, QString::fromStdString(currentReg), this), 12, 1, 1, 2);
-	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("IO Base Upper 16 Bits", width, height, QString::fromStdString(currentReg), this), 12, 3, 1, 2);
-
+	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
+	layout->addWidget(new CustomLineEdit("IO Limit Upper 16 Bits", width, height, QString::fromStdString(currentReg), this), 12, 1, 1, 2);
+	
 	layout->addWidget(new QLabel("034h"), 13, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Reserved", 3*width, height, "", this), 13, 1, 1, 3);
-	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("Capability Pointer", width, height, QString::fromStdString(currentReg), this), 13, 4, 1, 1);
+	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
+	layout->addWidget(new CustomLineEdit("Reserved", 3*width, height, "", this), 13, 1, 1, 3);
+	
 
 	layout->addWidget(new QLabel("038h"), 14, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
@@ -111,11 +119,13 @@ ConfigSpaceWidget::ConfigSpaceWidget(Type1Config* type1space , QWidget* parent)
 
 	layout->addWidget(new QLabel("03Ch"), 15, 0,1,1, Qt::AlignRight);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Bridge Control", 2*width, height, QString::fromStdString(currentReg), this), 15, 1, 1, 2);
+	layout->addWidget(new CustomLineEdit("Interrupt Line", width, height, QString::fromStdString(currentReg), this), 15, 4, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
 	layout->addWidget(new CustomLineEdit("Interrupt Pin", width, height, QString::fromStdString(currentReg), this), 15, 3, 1, 1);
 	boost::to_string(type1space->readType1Reg(regNum++), currentReg);
-	layout->addWidget(new CustomLineEdit("Interrupt Line", width, height, QString::fromStdString(currentReg), this), 15, 4, 1, 1);
+	layout->addWidget(new CustomLineEdit("Bridge Control", 2*width, height, QString::fromStdString(currentReg), this), 15, 1, 1, 2);
+	
+	
 
 	layout->setSpacing(0);
 
